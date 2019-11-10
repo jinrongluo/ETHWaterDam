55189d51b1c1752062e4fbaf0e88885253177307
JinRong Luo <kapoking@gmail.com>
2019-11-09 23:07:56 -0500

    deploy collector

--- contracts/ReadingCollector.sol
+++ contracts/ReadingCollector.sol
@@ -1,32 +1,55 @@
 pragma solidity ^0.4.22;
 import "./provableAPI.sol";
+import "./Integers.sol";
 
 contract ReadingCollector is usingProvable {
 
-   string[] public readings;
-   event LogConstructorInitiated(string nextStep);
-   event LogPriceUpdated(string price);
+   Reading[] public readings;
+   
+   uint public maxLevel;
+   uint public minLevel;
+   
+   int lastReading;
+   struct Reading {
+       uint256 time;
+       uint reading;
+       bool normal;
+   }
+   
+   event LogAbnormalReading(uint256 time, uint reading);
+   event LogConstructorInitiated(string description);
+   event LogReading(uint256 time, uint reading, bool normal);
    event LogNewProvableQuery(string description);
-
-   constructor() public payable {
-       emit LogConstructorInitiated("Constructor was initiated. Call 'updatePrice()' to send the Provable Query.");
+   
+   constructor() public {
+       maxLevel = 80;
+       minLevel = 20;
+       emit LogConstructorInitiated("Constructor was initiated. Water level max=80, min=20");
    }
 
    function __callback(bytes32 myid, string result) public {
        if (msg.sender != provable_cbAddress()) revert();
-       readings.push(result);
-       emit LogPriceUpdated(result);
-       
-       updatePrice();
+       uint256 time = now;
+       uint reading = Integers.parseInt(result);
+       bool normal = __checkReading(time, reading);
+       readings.push(Reading(time, reading, normal));
+       emit LogReading(time, reading, normal);
+       startReading();
    }
    
-   function updatePrice() public payable {
+   function startReading() public payable {
        if (provable_getPrice("URL") > this.balance) {
            emit LogNewProvableQuery("Provable query was NOT sent, please add some ETH to cover for the query fee");
        } else {
-           emit LogNewProvableQuery("Provable query was sent, standing by for the answer..");
            provable_query(10, "URL", "https://t22ujsj755.execute-api.us-east-1.amazonaws.com/default/smartmeter");
        }
-  
+   }
+   
+   function __checkReading(uint256 time, uint reading) private returns(bool) {
+       if(reading > maxLevel || reading < minLevel) {
+           emit LogAbnormalReading(time, reading);
+           return false;
+       }
+       return true;
    }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
 var ReadingCollector = artifacts.require("./ReadingCollector.sol");
+var Integers = artifacts.require("./Integers.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(ReadingCollector);
+async function doDeploy(deployer, network) {
+  await deployer.deploy(Integers);
+  await deployer.link(Integers, ReadingCollector);
+  await deployer.deploy(ReadingCollector);
+
+}
+
+module.exports = (deployer, network) => {
+  deployer.then(async () => {
+      await doDeploy(deployer, network);
+  });
 };
